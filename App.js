84d2d7515a6a0fae5a63390adf892b15ae55fc90
amolkapadi <72144987+amolkapadi@users.noreply.github.com>
2022-05-10 08:24:26 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+
+//var First_name = "Jay"; //this a first_name varible
+/*
+var First_name = "React js";
+First_name ="javascript";
+var lastName = "Patil";
+document.write(First_name);
+document.write(lastName);*/
+
+// let fname =" good morning";
+// //let fname ="good night";
+// fname ="react js";
+// document.write(fname);
+
+// const PI= 3.14;
+// //const PI=2.10;
+// PI=10;
+// document.write(PI);
+
+
+
+
+var a;
+a=document.getElementsByTagName('h1')[1];
+console.log(a);
+
